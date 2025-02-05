363270e0661c4dcace9662c3ca3fb3a73bbb500a
wang xi <1303780098@qq.com>
2025-02-05 22:15:51 +0800

    add css code

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -20,22 +20,21 @@
           <!-- 用 label 包裹后，点击里边任何一个元素都能触发 checkbox 的事件 -->
           <label>
             <input type="checkbox" />
-            Todo 1
-            <!-- 自定义的复选按钮 -->
+            HTML+CSS
             <span class="check-button"></span>
           </label>
         </div>
         <div class="todo-item">
           <label>
             <input type="checkbox" />
-            Todo 2
+            JavaScript
             <span class="check-button"></span>
           </label>
         </div>
         <div class="todo-item">
           <label>
             <input type="checkbox" />
-            Todo 3
+            Vue React
             <span class="check-button"></span>
           </label>
         </div>
@@ -51,12 +50,150 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Helvetica, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+/* 整个页面 */
+main {
+  width: 100vw;
+  min-height: 100vh;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  background: rgb(203, 210, 240);
+}
+
+.container {
+  width: 60%;
+  max-width: 400px;
+  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.15);
+  border-radius: 24px;
+  padding: 48px 28px;
+  background-color: rgb(245, 246, 252);
+}
+
+/* 标题 */
+h1 {
+  margin: 24px 0;
+  font-size: 28px;
+  color: #414873;
+}
+
+/* 添加框 */
+.input-add {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.input-add input {
+  padding: 16px 52px 16px 18px;
+  border-radius: 48px;
+  border: none;
+  outline: none;
+  box-shadow: 0px 0px 24px rgba(0, 0, 0, 0.08);
+  width: 100%;
+  font-size: 16px;
+  color: #626262;
+}
+
+.input-add button {
+  width: 46px;
+  height: 46px;
+  border-radius: 50%;
+  background: linear-gradient(#c0a5f3, #7f95f7);
+  border: none;
+  outline: none;
+  color: white;
+  position: absolute;
+  right: 0px;
+
+  cursor: pointer;
+}
+
+/* 通过纯css实现加号 */
+.input-add .plus {
+  display: block;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(#fff, #fff), linear-gradient(#fff, #fff);
+  background-size: 50% 2px, 2px 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.filters {
+  display: flex;
+  margin: 24px 2px;
+  color: #c0c2ce;
+  font-size: 14px;
+}
+
+.filters .filter {
+  margin-right: 14px;
+  transition: 0.8s;
+}
+
+.filters .filter.active {
+  color: #6b729c;
+  transform: scale(1.2);
+}
+
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+
+.todo-item {
+  background: white;
+  padding: 16px;
+  border-radius: 8px;
+  color: #626262;
+}
+
+.todo-item label {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+/* 选中item的动画特效 */
+.todo-item label span.check-button {
+  position: absolute;
+  top: 0;
+}
+
+.todo-item label span.check-button::before,
+.todo-item label span.check-button::after {
+  content: "";
+  display: block;
+  position: absolute;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+}
+
+.todo-item label span.check-button::before {
+  border: 1px solid #b382f9;
+}
+
+.todo-item label span.check-button::after {
+  transition: 0.4s;
+  background: #b382f9;
+  transform: translate(1px, 1px) scale(0.8);
+  opacity: 0;
+}
+
+.todo-item input {
+  margin-right: 16px;
+  opacity: 0;
+}
+
+.todo-item input:checked + span.check-button::after {
+  opacity: 1;
 }
 </style>
