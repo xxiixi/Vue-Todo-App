2761c6520d9cecbc67e913d70de103dee10291c7
wang xi <1303780098@qq.com>
2025-02-05 21:32:33 +0800

    add html code

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 
 `cd todo-app`
 `npm run serve`
+
+## Development Process Record
+
+### html part
+
+- 整体的容器:占满整个浏览器
+- todo 应用：水平居中在页面
+- App 标题
+- 添加 todo 的输入框和按钮
+- 过滤选项
+- todo 列表以及里边的项目
+- 每个项目有复选框和 todo 内容，以及白色容器
--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,17 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <div class="container">
+      <h1>xxiixi Todo-list</h1>
+      <!-- Add todo -->
+      <div class="input-add">
+        <input type="text" name="todo" />
+        <button>
+          <i class="plus"></i>
+        </button>
+      </div>
+      <!-- filters -->
+      <div class="filters">
+        <span class="filter active">All</span>
+        <span class="filter">Finished</span>
+        <span class="filter">Not Start</span>
+      </div>
+      <div class="todo-list">
+        <div class="todo-item">
+          <!-- 用 label 包裹后，点击里边任何一个元素都能触发 checkbox 的事件 -->
+          <label>
+            <input type="checkbox" />
+            Todo 1
+            <!-- 自定义的复选按钮 -->
+            <span class="check-button"></span>
+          </label>
+        </div>
+        <div class="todo-item">
+          <label>
+            <input type="checkbox" />
+            Todo 2
+            <span class="check-button"></span>
+          </label>
+        </div>
+        <div class="todo-item">
+          <label>
+            <input type="checkbox" />
+            Todo 3
+            <span class="check-button"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
